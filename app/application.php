e243340b8e7ab5c54ff919849285a8ff42505531
aligurbuz <galiant781@gmail.com>
2021-08-23 13:28:33 +0300

    application fix

--- app/application.php
+++ app/application.php
@@ -1,5 +1,11 @@
 #!/usr/bin/env php
 <?php
 
-exec('cd /var/www/html && chmod +x laravel.sh && ./laravel.sh');
-exec('cd /var/www/html/app/api && mv .env.example .env && chmod -R 777 storage && php artisan key:generate && php artisan migrate && php artisan passport:install && php artisan permission');
+if(!isset($argv[1])) exit();
+
+if(file_exists('/var/www/html/app/'.$argv[1].'/artisan')){
+    exit();
+}
+
+exec('cd /var/www/html/app && git clone https://github.com/aligurbuz/laravel-api.git '.$argv[1]);
+exec('cd /var/www/html/app/'.$argv[1].' && composer install && mv .env.example .env && chmod -R 777 storage && php artisan key:generate && php artisan name '.$argv[1].' && php artisan migrate && php artisan passport:install && php artisan permission');
