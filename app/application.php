c9339622de082b1a1398eb58f04f7665fff25ba4
aligurbuz <galiant781@gmail.com>
2021-08-25 14:19:53 +0300

    application supervisor fix

--- app/application.php
+++ app/application.php
@@ -8,4 +8,4 @@ if(file_exists('/var/www/html/app/'.$argv[1].'/artisan')){
 }
 
 exec('cd /var/www/html/app && git clone https://github.com/aligurbuz/laravel-api.git '.$argv[1]);
-exec('cd /var/www/html/app/'.$argv[1].' && composer install && mv .env.example .env && chmod -R 777 storage && php artisan key:generate && php artisan name '.$argv[1].' && php artisan migrate && php artisan passport:install && php artisan permission');
+exec('cd /var/www/html/app/'.$argv[1].' && composer install && mv .env.example .env && chmod -R 777 storage && php artisan key:generate && php artisan name '.$argv[1].' && php artisan migrate && php artisan passport:install && php artisan permission && php artisan supervisor');
